1b347552895dda3abae2d68946d7c07826b0353e
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-08-24 08:59:05 +0800

    Variables and Data Types

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,35 @@
-console.log('Hello World!!!');
+/******************************
+* Variables and data types
+*/
+
+var firstName = 'Erfi';
+console.log(firstName);
+
+var lastName = 'Anugrah';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+// Variable Naming Rules
+var _3years = 3;
+var johnMark = 'John and Mark';
+var if = 23;
+
+/******************************
+* Variable mutation and type coercion
+*/
+
+var firstName = 'Erfi';
+var age = 26;
+
+console.log(firstName + ' ' + age);
