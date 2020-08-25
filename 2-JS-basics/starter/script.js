019d85f8a67ba729c512bb4f28938b53172d171b
Erfi Anugrah <erfi.anugrah@gmail.com>
2020-08-25 09:05:59 +0800

    Basic operators

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,6 +1,5 @@
 /******************************
 * Variables and data types
-*/
 
 var firstName = 'Erfi';
 console.log(firstName);
@@ -24,10 +23,10 @@ console.log(job);
 var _3years = 3;
 var johnMark = 'John and Mark';
 // var if = 23;
+*/
 
 /******************************
 * Variable mutation and type coercion
-*/
 
 var firstName = 'Erfi';
 var age = 26;
@@ -48,3 +47,26 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + is
 
 var lastName = prompt('What is his last name?');
 console.log(firstName + ' ' + lastName);
+*/
+
+/******************************
+* Basic Operators
+*/
+
+var now, yearJohn, yearMark;
+now = 2020;
+ageJohn = 28;
+ageMark = 33;
+
+// Math Operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+console.log(yearJohn);
+console.log(yearMark);
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 2);
+
+// Logical Operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
